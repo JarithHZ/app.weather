13186ab70672e0bea2380bf5b240a62c93e3ada9
JarithHZ <162775009+JarithHZ@users.noreply.github.com>
2024-06-18 10:30:49 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,87 @@
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
+const notificationElement = document.querySelector(".notification");
+const feelsElement = document.querySelector(".feels");
+const temp_minElement = document.querySelector(".temp_min");
+const temp_maxElement = document.querySelector(".temp_max");
+const pressureElement = document.querySelector(".pressure");
+const humidityElement = document.querySelector(".humidity");
+const speedElement = document.querySelector(".speed");
+
+
+// App data
+const weather = {};
+weather.temperature = {
+    unit : "celsius"
+}
+
+// API KEY
+const key =  "1cdc1dd720a3ab365da903d6be442104";
+
+// check if the browser supports geolocation
+if('geolocation' in navigator){
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+} else {
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = "<p> Browser doesn't support Geolocation</p>"
+}
+
+// set user's position
+
+function setPosition(position){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    getWeather(latitude, longitude);
+}
+
+// show error when there is an issse with geolocation service
+function showError(error){
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = `<p> ${error.messange} </p>`;
+}
+
+function getWeather(latitude, longitude) {
+    let api = `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${key}`;
+    fetch(api)
+    .then(function (response) {
+    let data = response.json();
+    console.log(data);
+    return data;
+    })  
+    .then(function (data) {
+    weather.temperature.value = Math.floor(data.main.temp);
+    weather.description = data.weather[0].description;
+    weather.iconId = data.weather[0].icon;
+    weather.city = data.name;
+    weather.country = data.sys.country;
+    weather.feels = data.main.feels_like;
+    weather.temp_min = data.main.temp_min;
+    weather.temp_max = data.main.temp_max;
+    weather.pressure = data.main.pressure;
+    weather.humidity = data.main.humidity;
+    weather.speed = data.wind.speed;
+
+    })
+    .then(function() {
+    displayWeather();
+    });
+}
+
+
+
+// display weather to ui
+function displayWeather(){
+    iconElement.innerHTML = `<img src="${weather.iconId}.png"/>`;
+    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+    feelsElement.innerHTML = `<p>Feels like: </p>${weather.feels}`;
+    temp_minElement.innerHTML = `<p>Minimum temperature: </p>${weather.temp_min}`;
+    temp_maxElement.innerHTML = `<p>Maximum temperature: </p>${weather.temp_max}`;
+    pressureElement.innerHTML = `<p>Pressure: </p>${weather.pressure}`;
+    humidityElement.innerHTML = `<p>Humidity: </p>${weather.humidity}`;
+    speedElement.innerHTML = `<p>Speed: </p>${weather.speed}`;
+}
